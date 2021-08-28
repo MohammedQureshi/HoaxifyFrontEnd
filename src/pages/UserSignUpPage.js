1c3b39f786b81c0357d6e914583ec7ba67121dc9
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-08-28 10:42:13 +0100

    Add tests for other fields

--- src/pages/UserSignUpPage.js
+++ src/pages/UserSignUpPage.js
@@ -1,14 +1,42 @@
 import React from 'react'
 
 export class UserSignUpPage extends React.Component {
+
+    state = {
+        displayName: '',
+        username: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    onChangehDisplayName = (event) => {
+        const value = event.target.value;
+        this.setState({displayName: value});
+    }
+
+    onChangeUsername = (event) => {
+        const value = event.target.value;
+        this.setState({username: value});
+    }
+
+    onChangePassword = (event) => {
+        const value = event.target.value;
+        this.setState({password: value});
+    }
+
+    onChangeConfirmPassword = (event) => {
+        const value = event.target.value;
+        this.setState({confirmPassword: value});
+    }
+
     render(){
         return(
             <div>
                 <h1>Sign Up</h1>
-                <div><input placeholder="Your display name" /></div>
-                <div><input placeholder="Your username" /></div>
-                <div><input placeholder="Your password" type="password" /></div>
-                <div><input placeholder="Confirm your password" type="password" /></div>
+                <div><input placeholder="Your display name" value={this.state.displayName} onChange={this.onChangehDisplayName} /></div>
+                <div><input placeholder="Your username" value={this.state.username} onChange={this.onChangeUsername} /></div>
+                <div><input placeholder="Your password" type="password" value={this.state.password} onChange={this.onChangePassword} /></div>
+                <div><input placeholder="Confirm your password" type="password" value={this.state.confirmPassword} onChange={this.onChangeConfirmPassword} /></div>
                 <div><button>Sign Up</button></div>
             </div>
         )
--- src/pages/UserSignUpPage.spec.js
+++ src/pages/UserSignUpPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { UserSignUpPage } from './UserSignUpPage'
 
@@ -46,6 +46,39 @@ describe('UserSignUpPage', () => {
             const { container } = render(<UserSignUpPage />);
             const button = container.querySelector('button');
             expect(button).toBeInTheDocument();
+        })
+    })
+    describe('Interactions', () => {
+        const changeEvent = (content) => {
+            return {
+                target: {
+                    value: content
+                }
+            }
+        }
+        it('set the displayName value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const displayNameInput = queryByPlaceholderText('Your display name');
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'));
+            expect(displayNameInput).toHaveValue('my-display-name');
+        })
+        it('set the username value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const usernameInput = queryByPlaceholderText('Your username');
+            fireEvent.change(usernameInput, changeEvent('my-username-name'));
+            expect(usernameInput).toHaveValue('my-username-name');
+        })
+        it('set the password value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+            fireEvent.change(passwordInput, changeEvent('P4ssword'));
+            expect(passwordInput).toHaveValue('P4ssword');
+        })
+        it('set the confirm password value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />)
+            const passwordConfirm = queryByPlaceholderText('Confirm your password');
+            fireEvent.change(passwordConfirm, changeEvent('P4ssword'));
+            expect(passwordConfirm).toHaveValue('P4ssword');
         })
     })
 })
