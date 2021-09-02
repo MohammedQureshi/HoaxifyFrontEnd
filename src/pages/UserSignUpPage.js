5c55ded3d0f92756a09f48880e0097308d89fb57
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-09-02 22:15:58 +0100

    Update test for submit button

--- src/pages/UserSignUpPage.js
+++ src/pages/UserSignUpPage.js
@@ -29,17 +29,44 @@ export class UserSignUpPage extends React.Component {
         this.setState({confirmPassword: value});
     }
 
+    onClickSignUp = () => {
+        const user = {
+            username: this.state.username,
+            displayName: this.state.displayName,
+            password: this.state.password,
+        }
+        this.props.actions.postSignUp(user);
+    }
+
     render(){
         return(
-            <div>
-                <h1>Sign Up</h1>
-                <div><input placeholder="Your display name" value={this.state.displayName} onChange={this.onChangehDisplayName} /></div>
-                <div><input placeholder="Your username" value={this.state.username} onChange={this.onChangeUsername} /></div>
-                <div><input placeholder="Your password" type="password" value={this.state.password} onChange={this.onChangePassword} /></div>
-                <div><input placeholder="Confirm your password" type="password" value={this.state.confirmPassword} onChange={this.onChangeConfirmPassword} /></div>
-                <div><button>Sign Up</button></div>
+            <div className="container">
+                <h1 class="text-center">Sign Up</h1>
+                <div className="col-12 mb-3">
+                    <label>Display Name</label>
+                    <input placeholder="Your display name" value={this.state.displayName} onChange={this.onChangehDisplayName} className="form-control" />
+                </div>
+                <div className="col-12 mb-3">
+                    <label>Username</label>
+                    <input placeholder="Your username" value={this.state.username} onChange={this.onChangeUsername} className="form-control" />
+                </div>
+                <div className="col-12 mb-3">
+                    <label>Password</label>
+                    <input placeholder="Your password" type="password" value={this.state.password} onChange={this.onChangePassword} className="form-control" />
+                </div>
+                <div className="col-12 mb-3">
+                    <label>Confirm Password</label>
+                    <input placeholder="Confirm your password" type="password" value={this.state.confirmPassword} onChange={this.onChangeConfirmPassword} className="form-control" />
+                </div>
+                <div className="text-center"><button onClick={this.onClickSignUp} className="btn btn-primary w-100">Sign Up</button></div>
             </div>
         )
+    }
+}
+
+UserSignUpPage.defaultProps = {
+    actions: {
+        postSignUp: () => new Promise((resolve, reject) => {resolve({})})
     }
 }
 
--- src/pages/UserSignUpPage.spec.js
+++ src/pages/UserSignUpPage.spec.js
@@ -56,6 +56,27 @@ describe('UserSignUpPage', () => {
                 }
             }
         }
+
+        let button, displayNameInput, usernameInput, passwordInput, passwordConfirm;
+
+        const setUpForSubmit = (props) => {
+            const rendered = render(<UserSignUpPage {...props} />);
+            const {container, queryByPlaceholderText} = rendered;
+            
+            displayNameInput = queryByPlaceholderText('Your display name');
+            usernameInput = queryByPlaceholderText('Your username');
+            passwordInput = queryByPlaceholderText('Your password'); 
+            passwordConfirm = queryByPlaceholderText('Confirm your password');
+
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'));
+            fireEvent.change(usernameInput, changeEvent('my-username-name'));
+            fireEvent.change(passwordInput, changeEvent('P4ssword'));
+            fireEvent.change(passwordConfirm, changeEvent('P4ssword'));
+
+            button = container.querySelector('button');
+            return rendered
+        }
+
         it('set the displayName value into state', () => {
             const { queryByPlaceholderText } = render(<UserSignUpPage />);
             const displayNameInput = queryByPlaceholderText('Your display name');
@@ -79,6 +100,34 @@ describe('UserSignUpPage', () => {
             const passwordConfirm = queryByPlaceholderText('Confirm your password');
             fireEvent.change(passwordConfirm, changeEvent('P4ssword'));
             expect(passwordConfirm).toHaveValue('P4ssword');
+        })
+
+        it('calls postSignup when the field are valid and the actions are provided in props', () => {
+            const actions = {
+                postSignUp: jest.fn().mockResolvedValueOnce({})
+            }
+            setUpForSubmit({actions})
+            fireEvent.click(button)
+            expect(actions.postSignUp).toHaveBeenCalledTimes(1);
+        })
+
+        it('does not throw exception when clicking the button when actions not provided in props', () => {
+            setUpForSubmit()
+            expect(() => fireEvent.click(button)).not.toThrow();
+        })
+        
+        it('calls post with user body when the fields are valid', () => {
+            const actions = {
+                postSignUp: jest.fn().mockResolvedValueOnce({})
+            }
+            setUpForSubmit({actions})
+            fireEvent.click(button)
+            const expectedUserObject = {
+                username: 'my-username-name',
+                displayName: 'my-display-name',
+                password: 'P4ssword',
+            }
+            expect(actions.postSignUp).toHaveBeenCalledWith(expectedUserObject)
         })
     })
 })
